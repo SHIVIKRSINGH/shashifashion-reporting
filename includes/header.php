7809b31b5011808c33fc5d1d28e48089a0f4a380
SHIVIKRSINGH <shivendrasoftgen@gmail.com>
2025-07-23 14:53:55 +0530

    upd

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/config.php';
 
-if (session_status() === PHP_SESSION_NONE) session_start();
+// if (session_status() === PHP_SESSION_NONE) session_start();
 
 // Redirect if not logged in
 if (!isset($_SESSION['user_id'])) {
